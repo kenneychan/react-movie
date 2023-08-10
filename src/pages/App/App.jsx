5b790c7c3ed8f6e48243836085cd6d74f92d4367
Kenney Chan <kchan10@gmail.com>
2023-08-10 10:58:26 -0700

    cleanup

---
+++ src/pages/App/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Counter from "../../components/Counter.js";
+
+function App() {
+  return (
+    <main className="App">
+      <header className="App-header">
+        <h1>App</h1>
+      </header>
+      <Counter />
+    </main>
+  );
+}
+
+export default App;
