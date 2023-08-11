ef7cd5047f7e3c88cd48d72ffcbd09f503b0d824
Kenney Chan <kchan10@gmail.com>
2023-08-11 10:14:51 -0700

    Add auth page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,13 @@
 import "./AuthPage.css";
+import SigntUpForm from "../../components/SignUpForm/SignUpForm";
 
 export default function AuthPage() {
   return (
     <main className="AuthPage">
-      <h1>AuthPage</h1>
+      <main>
+        <h1>AuthPage</h1>
+        <SigntUpForm />
+      </main>
     </main>
   );
 }
